b8ca85c89c7fda17be733c8d5e6c612141362c49
22 <60903333+nini22P@users.noreply.github.com>
2025-02-24 23:10:31 +0800

    update assets url

--- src/files.ts
+++ src/files.ts
@@ -11,7 +11,7 @@ export const files: FileSystemTree = {
     "extract-zip": "^2.0.1"
   },
   "scripts": {
-    "start": "curl -o terre.zip https://gh-proxy.com/https://github.com/nini22P/WebGAL_Terre/releases/download/demo/webgal-terre-demo.zip && npx extract-zip terre.zip && rm -rf terre.zip && node main.js"
+    "start": "curl -o terre.zip https://gh-proxy.com/https://github.com/nini22P/webgal-terre-demo/releases/download/demo/webgal-terre-demo.zip && npx extract-zip terre.zip && rm -rf terre.zip && node main.js"
   }
 }`,
     },
